fd81c5ffa9cfa1ac5267df1e7a5c9b5f023072ee
Ayo Osho <aosho44@gmail.com>
2022-09-01 19:58:11 +0100

    It loads google

--- test/specs/test.js
+++ test/specs/test.js
@@ -1,15 +1,6 @@
-import LoginPage from  '../pageobjects/login.page';
-import SecurePage from '../pageobjects/secure.page';
-
-describe('My Login application', () => {
-    it('should login with valid credentials', async () => {
-        await LoginPage.open();
-
-        await LoginPage.login('tomsmith', 'SuperSecretPassword!');
-        await expect(SecurePage.flashAlert).toBeExisting();
-        await expect(SecurePage.flashAlert).toHaveTextContaining(
-            'You logged into a secure area!');
-    });
+describe('Finds highest rating course on Udemy website', () => {
+	it('loads the google website', async () => {
+		await browser.url('https://www.google.com/');
+		await expect(browser).toHaveUrl('https://www.google.com/');
+	});
 });
-
-
